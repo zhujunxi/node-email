aa6412e1b7aaa5118b1459c2d45078c44b7ece05
zhujunxi <531578783@qq.com>
2019-09-19 15:48:17 +0800

    feat: add get-weather API

---
+++ index.js
@@ -0,0 +1,31 @@
+const superagent = require("superagent");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const qs = require("qs")
+// const moment = require("moment");
+
+// 引入聚合数据(juhe.cn)的免费API
+const config = {
+    url: "http://apis.juhe.cn/simpleWeather/query",
+    city: "深圳",
+    key: "a642a2dbdc46e629058fd4e8f4e006a0"
+}
+function getWeatherInfo() {
+    return new Promise(function (resolve, reject) {
+        axios({
+            method: "get",
+            url: config.url + "?" + qs.stringify({
+                city: config.city,
+                key: config.key
+            })
+        }).then(function (res) {
+            resolve(res.data.result)
+        }).catch(function (err) {
+            reject(err)
+        })
+    })
+}
+
+getWeatherInfo().then(res => {
+    console.log(res);
+})
